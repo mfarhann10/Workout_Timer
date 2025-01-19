cf8f87c5f520b5218874f9a384496987b001a242
mfarhann10 <mfarhann1009@gmail.com>
2025-01-19 19:46:36 +0700

    add a helper useEffects for playing a sound

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { memo, useEffect, useState } from "react";
+import { memo, useCallback, useEffect, useState } from "react";
 import clickSound from "../ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -16,14 +16,20 @@ function Calculator({ workouts, allowSound }) {
     },
     [number, sets, speed, durationBreak]
   );
+
+  useEffect(
+    function () {
+      const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+      };
+      playSound();
+    },
+    [duration, allowSound]
+  );
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
-
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
 
   function handleInc() {
     setDuration((duration) => Math.floor(duration) + 1);
